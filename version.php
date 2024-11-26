12cd67dcfe94068b2cfd324b3b098946348a9b66
Anders Rasmussen <ara@praxis.dk>
2024-11-26 09:38:50 +0100

    https://github.com/praxisdigital/moodle-opgaver/issues/494 - Updated SVG viewbox

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->component = 'mod_smartlink';
-$plugin->version = 2024112500;
+$plugin->version = 2024112600;
 $plugin->release = '2.1.3';
 $plugin->requires = 2023042400; // Moodle 4.2
 $plugin->maturity = MATURITY_STABLE;
